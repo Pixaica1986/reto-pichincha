74d6d1af33e5339b80989ed54da8fd37c2f9cbf4
Luis Pottozen <pottozenpv1986@gmail.com>
2024-12-16 20:05:19 -0500

    [LPOTTOZEN][RELEASE] - Inicializacion de repositorio

---
+++ retopichincha/src/main/java/com/banco/app/retopichincha/RetopichinchaApplication.java
@@ -0,0 +1,13 @@
+package com.banco.app.retopichincha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetopichinchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetopichinchaApplication.class, args);
+    }
+
+}
---
+++ retopichincha/src/test/java/com/banco/app/retopichincha/RetopichinchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.app.retopichincha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetopichinchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
